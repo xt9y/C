5d582a7b696f36073b3b0563ffef7439d10eb487
Felix <felix@xt9y.de>
2026-08-12 17:57:45 +0200

    Build Raylib directly from source

--- examples/raylib/build.c
+++ examples/raylib/build.c
@@ -11,13 +11,33 @@ void build(C_Build *b) {
         "5.5"
     );
 
-    c_dep_cmake(raylib);
-    c_dep_link(raylib, "raylib");
-    c_dep_cmake_option(raylib, "-DBUILD_EXAMPLES=OFF");
+    c_dep_source(raylib);
+    c_dep_subdir(raylib, "src");
+    c_dep_include(raylib, ".");
+    c_dep_include(raylib, "external/glfw/include");
 
-#ifndef __APPLE__
-    c_dep_cmake_option(raylib, "-DGLFW_BUILD_WAYLAND=OFF");
-    c_dep_cmake_option(raylib, "-DGLFW_BUILD_X11=ON");
+    c_dep_sources(raylib, "rcore.c");
+    c_dep_sources(raylib, "rshapes.c");
+    c_dep_sources(raylib, "rtextures.c");
+    c_dep_sources(raylib, "rtext.c");
+    c_dep_sources(raylib, "utils.c");
+    c_dep_sources(raylib, "rmodels.c");
+    c_dep_sources(raylib, "raudio.c");
+    c_dep_sources(raylib, "rglfw.c");
+
+    c_dep_flag(raylib, "-std=gnu99");
+    c_dep_flag(raylib, "-D_GNU_SOURCE");
+    c_dep_flag(raylib, "-DPLATFORM_DESKTOP_GLFW");
+    c_dep_flag(raylib, "-DGRAPHICS_API_OPENGL_33");
+    c_dep_flag(raylib, "-Wno-missing-braces");
+    c_dep_flag(raylib, "-fno-strict-aliasing");
+
+#ifdef __APPLE__
+    c_dep_flag(raylib, "-x");
+    c_dep_flag(raylib, "objective-c");
+#else
+    c_dep_flag(raylib, "-D_GLFW_X11");
+    c_dep_flag(raylib, "-fPIC");
 #endif
 
     c_use(app, raylib);
@@ -26,6 +46,7 @@ void build(C_Build *b) {
     c_framework(app, "OpenGL");
     c_framework(app, "Cocoa");
     c_framework(app, "IOKit");
+    c_framework(app, "CoreAudio");
     c_framework(app, "CoreFoundation");
     c_framework(app, "CoreVideo");
 #else

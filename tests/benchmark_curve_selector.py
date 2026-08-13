fcee07eec8f79b54eed613a0b9ec3a0ed687a7cd
xt9y <felix@xt9y.de>
2026-08-14 01:04:47 +0200

    Point selector test at adaptive module

--- tests/benchmark_curve_selector.py
+++ tests/benchmark_curve_selector.py
@@ -6,7 +6,7 @@ import sys
 ROOT = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(ROOT / "benchmarks" / "sdl3"))
 
-from benchmark_curve import choose_ranges
+from curve_selector import choose_ranges
 
 
 def main():

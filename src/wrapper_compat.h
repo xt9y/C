7af8394a02a4c84642c40eef1a37c16231967cd3
xt9y <felix@xt9y.de>
2026-08-17 17:52:08 +0200

    Fix macOS wrapper compatibility without SDK headers

--- src/wrapper_compat.h
+++ src/wrapper_compat.h
@@ -2,23 +2,13 @@
 #define C_WRAPPER_COMPAT_H
 
 #ifdef __APPLE__
-#include <stddef.h>
-#include <sys/sysctl.h>
-#include <unistd.h>
-
-static inline long c_wrapper_online_cpus(int name) {
-    (void)name;
-    int logical_cpus = 1;
-    size_t size = sizeof(logical_cpus);
-    if (sysctlbyname("hw.logicalcpu", &logical_cpus, &size, NULL, 0) == 0 && logical_cpus > 0)
-        return logical_cpus;
-    return 1;
-}
-
 #ifndef _SC_NPROCESSORS_ONLN
-#define _SC_NPROCESSORS_ONLN 0
+#ifdef _SC_NPROCESSORS_CONF
+#define _SC_NPROCESSORS_ONLN _SC_NPROCESSORS_CONF
+#else
+#define _SC_NPROCESSORS_ONLN (-1)
 #endif
-#define sysconf(name) c_wrapper_online_cpus(name)
+#endif
 #endif
 
 #endif
